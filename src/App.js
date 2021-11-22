fcb26513ad62f344b724430d32b7922e79299a33
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-22 21:03:34 +0530

    waiting for auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} exact />
-          <Route path='/login' element={<Login />} exact />
-          <Route path='/signup' element={<Signup />} exact />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Fragment>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/login' element={<Login />} exact />
+            <Route path='/signup' element={<Signup />} exact />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </Fragment>
   );
 }
 
