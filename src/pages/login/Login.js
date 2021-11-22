8d8ef231106848d024ca34815325deface3aef39
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-22 16:23:08 +0530

    using login hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 import styles from "./Login.module.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    login(email, password);
   };
 
   return (
@@ -28,7 +31,13 @@ const Login = () => {
           value={password}
         />
       </label>
-      <button className='btn'>Login</button>
+      {!isPending && <button className='btn'>Login</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 };
