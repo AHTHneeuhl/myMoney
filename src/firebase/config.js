78e8b15518177f7acda1343e76a479ada82e5562
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-22 14:02:46 +0530

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVOEJOke55BVC1Hd5_0CkTOyhurUUVE5Q",
+  authDomain: "mymoney-a4575.firebaseapp.com",
+  projectId: "mymoney-a4575",
+  storageBucket: "mymoney-a4575.appspot.com",
+  messagingSenderId: "944040333983",
+  appId: "1:944040333983:web:a820ee991a30b6ddc5c8aa",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
