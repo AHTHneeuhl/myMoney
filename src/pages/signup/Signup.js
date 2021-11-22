7710e6ec028347a29212ae964d687d1f5bfa4822
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-22 13:50:19 +0530

    signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,45 @@
+import { useState } from "react/cjs/react.development";
 import styles from "./Signup.module.css";
 
 const Signup = () => {
-  return <div className={styles}>Signup</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Username </span>
+        <input
+          type='text'
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </label>
+      <label>
+        <span>Email </span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password </span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className='btn'>Sign Up</button>
+    </form>
+  );
 };
 
 export default Signup;
