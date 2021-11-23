687acc7246e5eb0115e3f1644d27fae5a5cd32e4
AHTHneeuhl <ahthneeuhl@gmail.com>
2021-11-23 13:14:51 +0530

    adding documents

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,6 +10,22 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return { isPending: true, document: null, success: false, error: null };
+    case "ADDED_DOCUMENT":
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -21,11 +37,30 @@ export const useFirestore = (collection) => {
 
   const ref = projectFirestore.collection(collection);
 
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add document
-  const addDocument = (doc) => {};
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const addedDocument = await ref.add(doc);
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (error) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: error.message });
+    }
+  };
 
   // delete document
-  const deleteDocument = (doc) => {};
+  const deleteDocument = async (doc) => {};
 
   useEffect(() => {
     return () => setIsCancelled(true);
